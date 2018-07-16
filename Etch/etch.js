c34f77d6626fdfadcdd26d0690d43e8532c3263e
Eric Dangerstone <reric2012@yahoo.com>
2018-07-15 21:25:38 -0500

    Adding buildGrid functionality and starting on mouseover effects

--- Etch/etch.js
+++ Etch/etch.js
@@ -1,17 +1,28 @@
-const container = document.querySelector("#grid");
-const div = document.createElement("div");
 
+const container = document.querySelector('#grid');
+let square = document.createElement('div');
+
+\\Enter grid x and y axis
 let first = parseInt(prompt("Enter x axis"));
 let second = parseInt(prompt("Enter y axis"));
+let third = first * second;
+
+\\Setting w & h factoring in square size and border thickness
+let newWidth = (first * 50) + (first * 2);
+let newHeight = second * 50 + (second * 2);
 alert("Building " + first + " by " + second + " grid.")
 
-function buildGrid(first=16,second=16){
 
-for(var i=0; i < (first*second); i++){
+\\Build grid using parameters above
+function buildGrid(){
+document.querySelector(".easel").style.width = newWidth  + "px";
+document.querySelector(".easel").style.height = newHeight + "px";
+for(var i=1; i < third + 1; i++){
   console.log(i);
-  container.appendChild(div);
-}
+  container.appendChild(square.cloneNode(true));
+}}
 
-}
+buildGrid();
 
-buildGrid(first,second);
+\\Create etch a sketch effect
+square.mouseover = doucment.square.style.background = black;
